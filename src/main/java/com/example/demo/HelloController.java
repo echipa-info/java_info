532e357c7d98ae84a5b4a3b0f4fe09148c11e369
luca400 <alexluca.predoi2018@gmail.com>
2022-02-15 17:17:42 +0200

    initial commit

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class HelloController {
+    @FXML
+    public TextField nr1;
+
+    @FXML
+    public TextField nr2;
+}
